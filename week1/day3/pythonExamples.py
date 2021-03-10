02563bdf8166fbbae8e1cb592cb2174f4575b9f3
Joseph Troina <jtroinacoding@gmail.com>
2021-03-10 11:34:41 -0500

    trying to save

---
+++ week1/day3/pythonExamples.py
@@ -0,0 +1,25 @@
+# Variable
+# nameOfVariable = assignedValueToVariable
+# String === "", "word", "1", "functionName"
+greeting = "Hello World"
+# Integer === whole number, 1, 23, 934234. 9392
+classSize = 23
+# Float === 4.5, 4.55565565, 4.3
+floatExample = 2.232
+# Boolean
+sumOfNumbers = 1 + 1
+# Name, assignment operator (=), value that I am assigning
+variablename = True
+# List, sequence of values followed by [value1, value2, etc]
+# [1, 2, 3]
+# ["strings", "string2", "blahblahblah"]
+# [False, True, False]
+# ["string", 1, 3.4, True, "string"]
+# nesting
+# ["list", ["list"]]
+# Dictionary
+# Sequence of labeled values {key: value}
+# {"stringName": "StringValue"}
+# {"FavoriteColor": "Red"}
+print("HelloWorld")
+print("My name is Joey and I am living the dream")
