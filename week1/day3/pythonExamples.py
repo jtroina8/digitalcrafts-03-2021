02c8d880623ad4cd5b373189db0c5398ac783e5e
Joseph Troina <jtroinacoding@gmail.com>
2021-03-10 12:35:11 -0500

    commiting it without worry

--- week1/day3/pythonExamples.py
+++ week1/day3/pythonExamples.py
@@ -4,15 +4,17 @@
 greeting = "Hello World"
 # Integer === whole number, 1, 23, 934234. 9392
 classSize = 23
+dirPlusTeacher = 4
 # Float === 4.5, 4.55565565, 4.3
 floatExample = 2.232
 # Boolean
-sumOfNumbers = 1 + 1
+sumOfTeachers = dirPlusTeacher + 1
 # Name, assignment operator (=), value that I am assigning
 variablename = True
 # List, sequence of values followed by [value1, value2, etc]
 somethingToCallIt = ["students", 23, True]
 somethingToCallIt = {"helloPythonWorld": 23, "we are students": 24.5}
+print(sumOfTeachers)
 # [1, 2, 3]
 # ["strings", "string2", "blahblahblah"]
 # [False, True, False]
