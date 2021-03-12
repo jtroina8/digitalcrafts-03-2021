019ca6864e580f83ee5d1b6169b34dfa1f33fecf
Joseph Troina <jtroinacoding@gmail.com>
2021-03-12 03:35:04 -0500

    uploading my hw

---
+++ week1/day4/listTuplesDictionaries.py
@@ -0,0 +1,21 @@
+# Lists
+# Contains stuff inside square brackets
+exampleList = ["list item one", 2, 3, "four"]
+
+# Dictionary
+# Sequence of LABELED values
+exampleOfDictionary = {"name": "Joey", "age": '26',
+                       "height": '6-2'. "hairHeight": "Long"}
+
+user = {
+    "name": "Joey"
+    "placesLived": {
+        "US": {"city": "Queens"},
+        "College": {Staten Island},
+    }
+    "favoriteFoods": ["spaghetti", "chinese", "burger", "cookies"]
+}
+
+# Tuples
+
+tupleExample = (33333, 44444)
