f9e50489ba88748e900e01fbcb57f8366e2c7a90
Joseph Troina <jtroinacoding@gmail.com>
2021-03-17 10:38:01 -0400

    class example for day 8

---
+++ week2/day7/buildingPractice.py
@@ -0,0 +1,17 @@
+# Create a building class
+# buildilng class will have
+# height
+# capacity
+# sqft
+# # commercial or residential
+# commercial
+# ask the user to create a building (new instance of the building class)
+# they will enter in height
+# capacity (people)
+# sqft
+# 5 instances of a building
+# print out the specs of the buildilng everytime a building is made
+# print statement needs to be a method
+
+class Building:
+    def __init__(self, heigh, capacity, sqft)
---
+++ week2/day8/inheritanceClasses.py
@@ -0,0 +1,8 @@
+class Currency:
+    def __init__(self, name, homeNationCurrency, price):
+        self.name = name,
+        self.homeNationCurrency = homeNationCurrency
+        self.price = price
+
+
+yen = Currency("Yen", "Japan", 20)
