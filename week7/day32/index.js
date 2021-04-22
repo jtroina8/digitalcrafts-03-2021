eaa2e8babeb1b9c8606c3ee968176f44cf403e0d
Joey <jtroinacoding@gmail.com>
2021-04-22 12:29:13 -0400

    staring the hw2

---
+++ week7/day32/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express();
+const { readFile } = require("fs")
+const PORT = 3002;
+
+app.use(express.json()); 
+
+app.get("/", (req, res) => {
+    const message = "This that homepage, dawg"; 
+        res.send(message);
+})
+
+app.get("/about", (req, res) => {
+    const message = "This that about page, dawg"; 
+        res.send(message);
+})
+
+app.get("/team", (req, res) => {
+    const message = "This that team page, dawg"; 
+        res.send(message);
+})
+
+
+app.post("/FAQ", (req, res) => {
+    const message = "This that FAQ, dawg"; 
+    res.send(message);
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Your server is being hosted on localhost:${PORT}`)
+})
