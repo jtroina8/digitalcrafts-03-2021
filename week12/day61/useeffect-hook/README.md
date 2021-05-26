d8f72ee2fd463f94caa2f8ded9000177ecda7b4f
Joey <jtroinacoding@gmail.com>
2021-05-26 10:02:32 -0400

    digitalcrafts push!`

--- week12/day61/useeffect-hook/README.md
+++ week12/day61/useeffect-hook/README.md
@@ -4,3 +4,23 @@
   = Create the ability to render dad jokes on the page.
 - Create the ability to render dad jokes on the page when the user hits submit.
 - Render components based off of how many times we hit submit.
+
+# Components
+
+- App . js
+- JokeContainer
+- joke
+
+  //componentDidMount
+  // this triggers everytime the component is updated or mounted
+  // useEffect(()=>{})
+  useEffect(() => {});
+
+//componentDidUpdate
+// this triggers when the component is mounted. then everytime the value inside the array changes
+// useEffect(()=>{},["value"])
+useEffect(() => {}, []);
+
+// this triggers one time only. when the component is mounted
+// useEffect(()=>{},[])
+useEffect(() => {}, []);
---
+++ week12/day63/react-router/src/App.js
@@ -0,0 +1,43 @@
+import "./App.css";
+import AboutMe from "./components/AboutMe";
+import Blog from "./components/Blog";
+import Home from "./components/Home";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <div>
+          <nav>
+            {<Link to="/">Home</Link>}
+            {<Link to="/aboutme">About Me</Link>}
+            {<Link to="/blog">Blog</Link>}
+          </nav>
+        </div>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route path="/aboutme">
+            <AboutMe />
+          </Route>
+          <Route path="/blog">
+            <Blog />
+          </Route>
+          <Route path="*">
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
